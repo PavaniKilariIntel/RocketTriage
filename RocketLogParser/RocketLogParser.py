2c2b81775212d12588da4905d10d691889c644cb
Pavani kilari <pavani.kilari@intel.com>
2024-05-19 00:12:10 -0500

    Code re-factoring and cleanup

--- RocketLogParser/RocketLogParser.py
+++ RocketLogParser/RocketLogParser.py
@@ -2,31 +2,34 @@
 import glob
 import sys
 
-uniqErrors = set()  # holds lines already seen
+unique_errors = set()  # holds lines already seen
 exclude_list = ["unregister", "fishing", "ignore", "Log Categories", "No error", "Caught PM error while fishing"]
 include_list = ["; PID(", "line #"]
 errors = 0
+total_log_files = 0
 
-def readLogFile(fileName):
+
+def read_log_file(file):
     global errors
-    with open(fileName, 'r') as f:
+    with open(file, 'r') as f:
         for line in f:
             line = line.strip().lower()
-            if ("error" in line or "fail" in line) and all(substring.lower() in line for substring in include_list) and not any(substring.lower() in line for substring in exclude_list):
+            if ("error" in line or "fail" in line) and all(
+                    substring.lower() in line for substring in include_list) and not any(
+                    substring.lower() in line for substring in exclude_list):
                 errors = errors + 1
                 # Following line generates complete error message,
                 # but we are interested in the Rocket application and error msg.
                 # uniqErrors.add(f.name.split('\\')[-1] + ", " + line.strip().replace(";", ","))
-                uniqErrors.add( (f.name.split('\\')[-1]).split("_")[0] + ":" + line.replace(";", ",").split(",")[-1])
+                unique_errors.add((f.name.split('\\')[-1]).split("_")[0] + ":" + line.replace(";", ",").split(",")[-1])
     # print(f"TOTAL ERRORS IN THE LOG {fileName} FILES :: ", errors)
 
 
-def printErrors():
-    linenum = 0
-    for errorstr in sorted(uniqErrors):
-        linenum = linenum + 1
-        print(errorstr)
-    print("Total Unique Errors found :: ", linenum)
+def print_errors():
+    print(*unique_errors, sep="\n")
+    print("TOTAL LOG FILES SCANNED  ::: ", total_log_files)
+    print("TOTAL ERRORS LOGGED      ::: ", errors)
+    print("TOTAL UNIQUE PATTERNS    ::: ", unique_errors.__len__())
 
 
 def main():
@@ -41,15 +44,14 @@ def main():
     #     direc = "C:/Pavani/Projects/ACE/GENAI/Triage/Rocket/ROCKET_LOGS/Archive/EXTRACTED/logs/"
     #     print("The directory is empty, assign default path :: ", direc)
 
-    x = 0
+    global total_log_files
+    files = glob.glob(direc + '*.log')
+    total_log_files = files.__len__()
 
     # print('Named with wildcard *.log :')
-    for files in glob.glob(direc + '*.log'):
-        x = x + 1
-        readLogFile(files)
-    printErrors()
-    print("TOTAL ERRORS LOCATED :: ", errors)
-    print("TOTAL LOG FILES ::: ", x)
+    for file in files:
+        read_log_file(file)
+    print_errors()
 
 
 if __name__ == "__main__":
