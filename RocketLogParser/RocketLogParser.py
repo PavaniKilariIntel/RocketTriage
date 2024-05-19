f2f4a1a005106ae1d7a44e76195380e0f8bb59c2
Pavani kilari <pavani.kilari@intel.com>
2024-05-18 21:32:14 -0500

    Git ignore file and code cleanup.

--- RocketLogParser/RocketLogParser.py
+++ RocketLogParser/RocketLogParser.py
@@ -3,7 +3,7 @@ import glob
 import sys
 
 uniqErrors = set()  # holds lines already seen
-exclude_list = ["unregister", "fishing", "ignor", "Log Categories", "No error", "Caught PM error while fishing"]
+exclude_list = ["unregister", "fishing", "ignore", "Log Categories", "No error", "Caught PM error while fishing"]
 include_list = ["; PID(", "line #"]
 
 
@@ -11,27 +11,21 @@ def readLogFile(fileName):
     errors = 0
     with open(fileName, 'r') as f:
         for line in f:
-            # if "error" in line and not any(substring.lower() in line.lower() for substring in exclude_list):
-            #     errors = errors + 1
-            #     # if line not in (None, '') or not line.strip():
-            #     uniqErrors.add(f.name.split('\\')[-1] +  " --> " + line.strip())
             if ("error" in line.lower() or "fail" in line.lower()) and all(
                     substring.lower() in line.lower() for substring in include_list) and not any(
                     substring.lower() in line.lower() for substring in exclude_list):
                 errors = errors + 1
-                # if line not in (None, '') or not line.strip():
                 uniqErrors.add(f.name.split('\\')[-1] + " --> " + line.strip())
-                # print(line)
     # print(f"TOTAL ERRORS IN THE LOG {fileName} FILES :: ", errors)
 
 
 def printErrors():
-    lineNum = 0
+    linenum = 0
     sorted(uniqErrors)
     for errors in uniqErrors:
-        lineNum = lineNum + 1
+        linenum = linenum + 1
         print(errors)
-    print("Total Unique Errors found :: ", lineNum)
+    print("Total Unique Errors found :: ", linenum)
 
 
 def main():
@@ -48,14 +42,11 @@ def main():
 
     x = 0
 
-    print('\nNamed with wildcard *.log :')
+    print('Named with wildcard *.log :')
     for files in glob.glob(direc + '*.log'):
         x = x + 1
-        # print(files)
         readLogFile(files)
-
     printErrors()
-
     print("TOTAL LOG FILES ::: ", x)
 
 
